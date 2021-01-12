c0a751bce3de62251ad54dba79ca371cb5e26d26
Daniel Hoherd <daniel.hoherd@gmail.com>
2021-01-12 14:13:09 -0800

    Start 12to13 work

--- versions.tf
+++ versions.tf
@@ -1,3 +1,14 @@
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 0.13"
+  required_providers {
+    helm = {
+      source = "hashicorp/helm"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+    }
+    null = {
+      source = "hashicorp/null"
+    }
+  }
 }
